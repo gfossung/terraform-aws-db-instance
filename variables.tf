44e5328efa08e82a348a15f7175a6e60a59e2381
gfossung <42151386+gfossung@users.noreply.github.com>
2024-11-17 15:22:15 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "ami" {
+   type = string
+   default = "ami-0d64bb532e0502c46"
+}
+
+variable "az" {
+   type = string
+   default = "eu-west-1a" 
+}
+
+variable "dbport" {
+   type = number
+   default = 3306 
+}
+
+variable "typeofinstance" {
+   type = string
+   default = "t2.micro" 
+}
+
+variable "dbsize" {
+   type = number
+   default = 10
+}
+
+variable "keyname" {
+   type = string
+   default = "jenkins" 
+}
+
